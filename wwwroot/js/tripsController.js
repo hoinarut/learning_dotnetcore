0528474f4e0a21be31224a79f4153fdb7ece09f1
hoinarut <tudor.hoinaru@gmail.com>
2017-06-07 10:06:58 +0300

    Adding Angular 1

---
+++ wwwroot/js/tripsController.js
@@ -0,0 +1,22 @@
+(function () {
+    angular.module("app-trips")
+        .controller("tripsController", tripsController);
+
+    function tripsController() {
+        var vm = this;
+
+        vm.trips = [{
+            name: "US Trip",
+            created: new Date()
+        }, {
+            name: "World Trip",
+            created: new Date()
+        }];
+
+        vm.newTrip = {};
+
+        vm.addTrip = function () {
+            alert(vm.newTrip.name);
+        };
+    }
+})();
